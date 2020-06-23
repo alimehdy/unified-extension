ac080240cb89f532f77c534792a6c814ff043efc
im_officer_leb <ali.mehdy@medair.org>
2020-06-23 12:05:24 +0300

    Fixing api.service isDevMode script

--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -4,8 +4,6 @@ import { HttpHeaders } from '@angular/common/http';
 import { isDevMode } from '@angular/core';
 import { timeout, catchError } from 'rxjs/operators';
 import { throwError } from 'rxjs';
-
-
 const dhisUser = 'Ali.Mehdy';
 const dhisPassword = 'LBN22v10!LBN22v10';
 
@@ -15,58 +13,59 @@ const onaPassword = 'LBNIM22v10';
 let dhisHttpOptions;
 let onaHttpOptions;
 
-if (isDevMode()) {
-  dhisHttpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: 'Basic ' + btoa(dhisUser + ':' + dhisPassword),
-      'Access-Control-Allow-Origin': '*',
-      // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
-      'access-control-allow-credentials': 'true'
-
-    })
-  };
-  onaHttpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      Authorization: 'Basic ' + btoa(onaUser + ':' + onaPassword),
-    })
-  };
-  // onaHttpOptions = {
-  //   headers: new HttpHeaders({
-  //     'Content-Type':  'application/json',
-  //     Authorization: 'Basic ' + btoa(onaUser + ':' + onaPassword),
-  //     'Access-Control-Allow-Origin': '*',
-  //     // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
-  //     'access-control-allow-credentials': 'true'
-  //   })
-  // };
-} else {
-  dhisHttpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
-      'access-control-allow-credentials': 'true'
-
-    })
-  };
-  onaHttpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
-      'access-control-allow-credentials': 'true'
-
-    })
-  };
-}
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {
+    if (isDevMode()) {
+      dhisHttpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Basic ' + btoa(dhisUser + ':' + dhisPassword),
+          'Access-Control-Allow-Origin': '*',
+          // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
+          'access-control-allow-credentials': 'true'
+
+        })
+      };
+      onaHttpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          Authorization: 'Basic ' + btoa(onaUser + ':' + onaPassword),
+        })
+      };
+      // onaHttpOptions = {
+      //   headers: new HttpHeaders({
+      //     'Content-Type':  'application/json',
+      //     Authorization: 'Basic ' + btoa(onaUser + ':' + onaPassword),
+      //     'Access-Control-Allow-Origin': '*',
+      //     // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
+      //     'access-control-allow-credentials': 'true'
+      //   })
+      // };
+    } else {
+      dhisHttpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
+          'access-control-allow-credentials': 'true'
+
+        })
+      };
+      onaHttpOptions = {
+        headers: new HttpHeaders({
+          'Content-Type': 'application/json',
+          'Access-Control-Allow-Origin': '*',
+          // 'Access-Control-Allow-Methods': 'POST, GET, OPTIONS, DELETE, PUT',
+          'access-control-allow-credentials': 'true'
+
+        })
+      };
+    }
+  }
 
   getData(url, orgApi) {
     const httpOptions = orgApi === 'ona' ? onaHttpOptions : dhisHttpOptions;
